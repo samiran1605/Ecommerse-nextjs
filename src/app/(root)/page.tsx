8250a4276043065dec13c8409abc71bc60e08bff
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-11-07 20:51:16 +0530

    first commit + clerk authentication

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const SetupPage = () => {
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default SetupPage;
