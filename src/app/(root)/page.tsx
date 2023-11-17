8515ff991021669aa9f9d45f5e620ce910956af3
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-11-17 20:18:33 +0530

    added provider

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModel } from "@/hooks/use-store-model";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  return (
-    <div className="p4">
-      <Modal title="test" description="test desc" isOpen onClose={() => {}}>
-        Children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModel((state) => state.onOpen);
+  const isOpen = useStoreModel((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p4">Root Page</div>;
 };
 
 export default SetupPage;
--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useStoreModel } from "@/hooks/use-store-model";
-import { Modal } from "../ui/modal";
+import { Modal } from "@/components/ui/modal";
 
 export const StoreModal = () => {
   const storeModal = useStoreModel();
